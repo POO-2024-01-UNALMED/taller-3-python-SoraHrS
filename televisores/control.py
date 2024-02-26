bde193f2908ea43b177a7eb0dfe66e6f81402eff
SoraHrS <soherreras@unal.edu.co>
2024-02-26 12:36:48 -0500

    Se crearon las clases Control, Marca, Tv

---
+++ televisores/control.py
@@ -0,0 +1,38 @@
+class Control:
+    def __init__(self):
+        self.__tv 
+
+    def enlazar(self, tv):
+        self.setTv(tv)
+        self.__tv.setControl(self)
+
+    def getTv(self):
+        return self.__tv
+
+    def setTv(self, tv):
+        self.__tv = tv
+
+    def turnOn(self):
+        self.__tv.turnOn()
+
+    def turnOff(self):
+        self.__tv.turnOff()
+
+    def canalUp(self):
+        self.__tv.canalUp()
+
+    def canalDown(self):
+        self.__tv.canalDown()
+
+    def volumenUp(self):
+        self.__tv.volumenUp()
+
+    def volumenDown(self):
+        self.__tv.volumenDown()
+    
+    def setCanal(self, canal):
+        self.__tv.setCanal(canal)
+
+    def setVolumen(self, volumen):
+        self.__tv.setVolumen(volumen)
+        
---
+++ televisores/tv.py
@@ -0,0 +1,72 @@
+class TV:
+    __numTV
+    def __init__ (self, marca, estado):
+        self.__marca = marca
+        self.__canal = 1
+        self.__precio = 1
+        self.__estado = estado
+        self.__volumen = 500
+        self.__control
+        cls.__numTV += 1
+
+    def getMarca(self):
+        return self.__marca
+    
+    def getCanal(self):
+        return self.__canal
+
+    def getPrecio(self):
+        return self.__precio
+    
+    def getVolumen(self):
+        return self.__volumen
+
+    def getControl(self):
+        return self.__control
+
+    def setMarca(self, marca):
+        self.__marca = marca
+    
+    def setCanal(self, canal):
+        if estado and canal>=1 and canal<=120:
+            self.__canal = canal
+
+    def setPrecio(self, precio):
+        self.__precio = precio
+    
+    def setVolumen(self, volumen):
+        if estado and canal>=0 and canal<=7:
+            self.__volumen = volumen
+
+    def setControl(self, control):
+        self.__control = control
+
+    @classmethod
+    def getNumTv(cls):
+        return cls.__numTV
+
+    @classmethod
+    def setNumTv(cls, num):
+        cls.__numTV = num
+    
+    def turnOn(self):
+        self.__estado = True
+
+    def turnOff(self):
+        self.__estado = False
+
+    def getEstado(self):
+        return self.__estado
+
+    def canalUp(self):
+        self.setCanal(self.__canal +1)
+
+    def canalDown(self):
+        self.setCanal(self.__canal -1)
+
+    def volumenUp(self):
+        self.setVolumen(self.__volumen +1)
+
+    def volumenDown(self):
+        self.setVolumen(self.__volumen -1)
+        
